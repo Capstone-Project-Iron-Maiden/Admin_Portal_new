fcd0553d5740994939871f96ab8d207e76cfd668
Niya Anil <niyapanil@gmail.com>
2020-02-29 16:12:43 -0500

    Event Register Commit

--- js/script.js
+++ js/script.js
@@ -32,3 +32,11 @@ function readURL(input) {
   $("#imageUpload").change(function () {
     readURL(this);
   }); 
+
+  $('#file-upload').change(function() {
+    var filepath = this.value;
+    var m = filepath.match(/([^\/\\]+)$/);
+    var filename = m[1];
+    $('#filename').html(filename);
+
+});
