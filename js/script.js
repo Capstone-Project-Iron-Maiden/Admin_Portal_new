2884cd0bff3478747f620c4f2ef15b2ce94b904e
Niya Anil <niyapanil@gmail.com>
2020-02-29 10:59:53 -0500

    first commit

---
+++ js/script.js
@@ -0,0 +1,34 @@
+
+
+
+$(document).ready(function(){
+    // var adjustSidebar = function(){
+    //     $(".sidebar").slimScroll({
+    //         height: document.documentElement.clientHeight - $(".navbar").outerHeight()
+    //     })(jQuery);
+    // };
+    // adjustSidebar();
+
+    // $(window).resize(function(){
+    //     adjustSidebar();
+    // })
+    $(".sideMenuToggler").on("click", function(){
+        $(".wrapper").toggleClass("active");
+
+    });
+});
+
+function readURL(input) {
+    if (input.files && input.files[0]) {
+      var reader = new FileReader();
+      reader.onload = function (e) {
+        $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
+        $('#imagePreview').hide();
+        $('#imagePreview').fadeIn(650);
+      }
+      reader.readAsDataURL(input.files[0]);
+    }
+  }
+  $("#imageUpload").change(function () {
+    readURL(this);
+  }); 
