e6c8b1e45311098ed909f3397a58e100c2f0eb0f
Niya Anil <niyapanil@gmail.com>
2020-03-21 01:18:37 -0400

    speaker change

--- js/script.js
+++ js/script.js
@@ -18,21 +18,18 @@ $(document).ready(function(){
     });
 });
 
-function readURL(input) {
-    if (input.files && input.files[0]) {
-      var reader = new FileReader();
-      reader.onload = function (e) {
-        $('#imagePreview').css('background-image', 'url(' + e.target.result + ')');
-        $('#imagePreview').hide();
-        $('#imagePreview').fadeIn(650);
-      }
-      reader.readAsDataURL(input.files[0]);
-    }
-  }
-  $("#imageUpload").change(function () {
-    readURL(this);
-  }); 
-
+// function readURL(input) {
+//   if (input.files && input.files[0]) {
+//       var reader = new FileReader();
+//       reader.onload = function(e) {
+//           $('#imagePreview').css('background-image', 'url('+e.target.result +')');
+//       }
+//       reader.readAsDataURL(input.files[0]);
+//   }
+// }
+// $("#imageUpload").change(function() {
+//   readURL(this);
+// });
   $('#file-upload').change(function() {
     var filepath = this.value;
     var m = filepath.match(/([^\/\\]+)$/);
